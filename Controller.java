01e4c1f9f55bbca4ef975559e0b03a304b79ee60
JavierCR16 <javier.contrerasm16@gmail.com>
2017-05-13 02:16:37 -0600

    Add files via upload
    ddfgdf

---
+++ Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    public void initialize(URL fxmlLocations, ResourceBundle resources){}
+}
